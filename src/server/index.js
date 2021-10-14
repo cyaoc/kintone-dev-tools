ec3494d041de39d478f00d0af7cfaa1d09413daa
cyao <cyao@cybozu.cn>
2021-10-14 18:14:32 +0800

    fixed node api‘s bug

--- src/server/index.js
+++ src/server/index.js
@@ -1,15 +1,16 @@
 const https = require('https')
 const express = require('express')
 const serveIndex = require('serve-index')
+const ip = require('ip')
 const { certificateFor } = require('../cert')
 const logger = require('../logger')
 
-module.exports.start = (dir) => {
+module.exports.start = (option) => {
   logger.info('正在配置开发者证书')
-  const cert = certificateFor()
+  const myip = ip.address()
+  const cert = certificateFor(myip)
   const app = express()
-  app.use('/', express.static(dir), serveIndex(dir, { icons: true }))
-
+  app.use('/', express.static(option.static), serveIndex(option.static, { icons: true }))
   const httpsServer = https.createServer(
     {
       key: cert.private,
@@ -17,6 +18,9 @@ module.exports.start = (dir) => {
     },
     app,
   )
-  httpsServer.listen(443)
-  logger.info('服务器已经启动 https://localhost')
+  httpsServer.listen(option.port ? option.port : 443)
+  logger.info('服务器已经启动,你可以通过以下链接来访问:')
+  const port = !option.port || option.port === 443 ? '' : `:${option.port}`
+  logger.info(`本地 -> https://localhost${port}/`)
+  logger.info(`局域网 -> https://${myip}${port}/`)
 }
